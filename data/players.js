34948e6f0cddd304a34e0d5f71316bda82ad22e2
mart1d4 <mart1danton@gmail.com>
2024-01-02 23:26:55 +0100

    meow

--- data/players.js
+++ data/players.js
@@ -7,6 +7,43 @@ const players = [
         username: "Qwaark",
         discordId: "867093779844431922",
     },
+    {
+        username: "ASH3R",
+        discordId: "497840521964945408",
+    },
+    {
+        username: "FalconNew",
+        discordId: "345220098098987010",
+    },
+    {
+        username: "Filourout",
+        discordId: "652536798744150016",
+    },
+    {
+        username: "HULK26",
+        discordId: "469949346633220106",
+        minTroops: 10000,
+    },
+    {
+        username: "ic4edges",
+        discordId: "1168133872836415538",
+    },
+    {
+        username: "Looz",
+        discordId: "494978884761157642",
+    },
+    {
+        username: "Gandalf.",
+        discordId: "312679316645216257",
+    },
+    {
+        username: "Reddington",
+        discordId: "444174409071067137",
+    },
+    {
+        username: "Torkadess",
+        discordId: "497031157402042369",
+    },
 ];
 
 module.exports = players;
--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const { XMLParser } = require("fast-xml-parser");
 const { sanitize } = require("./lib/string");
 const WebSocket = require("ws");
 require("dotenv").config();
+const fs = require("fs");
 
 const players = require("./data/players.js");
 const soldiers = require("./data/soldiers.js");
 const tools = require("./data/tools.js");
+const { login, displayMessage } = require("./lib/functions.js");
 
 const username = process.env.USERNAMEE;
 const password = process.env.PASSWORD;
@@ -19,12 +21,63 @@ if (!username || !password || !accountId || !allianceId) {
 
 const servers = {};
 getServers();
-setTimeout(getServers, 3600000);
 
+const webhookUrlAttacks = process.env.WEBHOOK_URL_ATTACKS;
+const webhookUrlAllianceChat = process.env.WEBHOOK_URL_ALLIANCE_CHAT;
+const webhookUrlAllianceLogs = process.env.WEBHOOK_URL_ALLIANCE_LOGS;
 const allianceLogsRead = [];
 let alreadyLoggedFirstAllianceLogs = false;
 
 const messages = [];
+
+// getUnits();
+
+async function getUnits() {
+    const versionUrl =
+        "https://empire-html5.goodgamestudios.com/default/items/ItemsVersion.properties";
+    const versionFile = await fetch(versionUrl).then((res) => res.text());
+
+    const version = versionFile.split("=")[1].trim();
+
+    const itemsUrl = `https://empire-html5.goodgamestudios.com/default/items/items_v${version}.json`;
+    const itemsFile = await fetch(itemsUrl).then((res) => res.json());
+    const units = itemsFile.units;
+
+    const tools = units.filter((unit) => !!unit.typ);
+    const soldiers = units.filter((unit) => !unit.typ);
+
+    const soldiersFile = fs.createWriteStream("./data/soldiers.js");
+    // Create array named soldiers with all soldiers and export it
+    soldiersFile.write(`const soldiers = [\n`);
+    soldiers.forEach((soldier) => {
+        if (soldier.level) {
+            soldiersFile.write(
+                `    { id: ${soldier.wodID}, name: "${soldier.comment2}", level: "${soldier.level}" },\n`
+            );
+        } else {
+            soldiersFile.write(`    { id: ${soldier.wodID}, name: "${soldier.comment2}" },\n`);
+        }
+    });
+
+    soldiersFile.write(`];\n\nmodule.exports = soldiers;`);
+    soldiersFile.end();
+
+    const toolsFile = fs.createWriteStream("./data/tools.js");
+    // Create array named tools with all tools and export it
+    toolsFile.write(`const tools = [\n`);
+    tools.forEach((tool) => {
+        if (tool.level) {
+            toolsFile.write(
+                `    { id: ${tool.wodID}, name: "${tool.comment2}", level: "${tool.level}" },\n`
+            );
+        } else {
+            toolsFile.write(`    { id: ${tool.wodID}, name: "${tool.comment2}" },\n`);
+        }
+    });
+
+    toolsFile.write(`];\n\nmodule.exports = tools;`);
+    toolsFile.end();
+}
 
 async function getServers() {
     const serversUrl = "https://empire-html5.goodgamestudios.com/config/network/1.xml";
@@ -49,60 +102,36 @@ function connect(header) {
     const socket = servers[header].socket;
 
     socket.addEventListener("open", async (event) => {
-        socket.send(`<msg t='sys'><body action='verChk' r='0'><ver v='166' /></body></msg>`);
-
-        socket.send(
-            `<msg t='sys'><body action='login' r='0'><login z='EmpireEx_3'><nick><![CDATA[]]></nick><pword><![CDATA[1093006%en%0]]></pword></login></body></msg>`
-        );
-
-        socket.send(`<msg t='sys'><body action='autoJoin' r='-1'></body></msg>`);
-        socket.send(`<msg t='sys'><body action='roundTrip' r='1'></body></msg>`);
-
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-
-        socket.send(
-            `%xt%EmpireEx_3%vck%1%1093006%web-html5%<RoundHouseKick>%1.6082669484889843e+308%`
-        );
-
-        socket.send(`%xt%EmpireEx_3%vln%1%{"NOM":"${username}"}%`);
-
-        socket.send(
-            `%xt%EmpireEx_3%lli%1%{"CONM":112,"RTM":20,"ID":0,"PL":1,"NOM":"${username}","PW":"${password}","LT":null,"LANG":"en","DID":"0","AID":"${allianceId}","KID":"","REF":"https://empire.goodgamestudios.com","GCI":"","SID":9,"PLFID":1}%`
-        );
-
+        login(socket, username, password, allianceId);
         await new Promise((resolve) => setTimeout(resolve, 1000));
 
         // Check alliance logs every minute
-
         socket.send(`%xt%EmpireEx_3%all%1%{}%`);
-
-        setTimeout(() => {
-            socket.send(`%xt%EmpireEx_3%all%1%{}%`);
-        }, 60000);
+        setInterval(() => socket.send(`%xt%EmpireEx_3%all%1%{}%`), 60000);
     });
 
     socket.addEventListener("message", async (event) => {
-        const eventData = event.data.toString().split("%");
+        const message = event.data.toString().split("%");
 
-        const response = {
+        const data = {
             server: header,
-            command: eventData[2],
-            code: eventData[4],
-            content: JSON.parse(JSON.stringify(eventData[5] || "")),
+            command: message[2],
+            code: message[4],
+            content: JSON.parse(JSON.stringify(message[5] || "{}")),
         };
 
-        if (response.command === "lli") {
-            if (response.code === "0") {
+        if (data.command === "lli") {
+            if (data.code === "0") {
                 pingSocket(socket, header);
-            } else if (response.code === "21") {
+            } else if (data.code === "21") {
                 socket.send(
                     `%xt%${header}%lre%1%{"DID":0,"CONM":515,"RTM":60,"campainPId":-1,"campainCr":-1,"campainLP":-1,"adID":-1,"timeZone":14,"username":"${username}","email":null,"password":"${password}","accountId":"${accountId}","ggsLanguageCode":"en","referrer":"https://empire.goodgamestudios.com","distributorId":0,"connectionTime":515,"roundTripTime":60,"campaignVars":";https://empire.goodgamestudios.com;;;;;;-1;-1;;1681390746855129824;0;;;;;","campaignVars_adid":"-1","campaignVars_lp":"-1","campaignVars_creative":"-1","campaignVars_partnerId":"-1","campaignVars_websiteId":"0","timezone":14,"PN":"${username}","PW":"${password}","REF":"https://empire.goodgamestudios.com","LANG":"fr","AID":"${allianceId}","GCI":"","SID":9,"PLFID":1,"NID":1,"IC":""}%`
                 );
             } else {
                 socket.close();
             }
-        } else if (response.command === "lre") {
-            if (response.code === "0") {
+        } else if (data.command === "lre") {
+            if (data.code === "0") {
                 pingSocket(socket, header);
             } else {
                 servers[header].reconnect = false;
@@ -110,9 +139,10 @@ function connect(header) {
             }
         }
 
-        if (response.command === "sne") {
+        if (data.command === "sne") {
+            return;
             // Private message
-            const content = JSON.parse(response.content);
+            const content = JSON.parse(data.content);
             const messageId = content["MSG"][0][0];
 
             messages.push({
@@ -120,23 +150,27 @@ function connect(header) {
                 title: content["MSG"][0][2],
             });
 
+            // Send request to read message
             socket.send(`%xt%EmpireEx_3%rms%1%{"MID":${messageId}}%`);
         }
 
-        if (response.command === "rms") {
+        if (data.command === "rms") {
+            return;
             // Read message
-            const content = JSON.parse(response.content);
+            const content = JSON.parse(data.content);
             const message = sanitize(content["MTXT"]);
             const title = messages.find((message) => message.id === content["MID"]).title;
 
-            console.log(`Message: ${title}\n${message}`);
+            // Could also get the sender's name
+            // Would also be great to get special messages like war announcements
+
+            displayMessage(title, message);
         }
 
-        if (response.command === "gam") {
-            const currentAllianceId = 163094;
-            const currentAllianceName = "THE INSANES";
-
-            const content = JSON.parse(response.content);
+        if (data.command === "gam") {
+            // Attack / Support
+            const content = JSON.parse(data.content);
+            const currentAllianceName = "THE INSANES"; // TODO: get alliance name programmatically
 
             const firstPlayer = content["O"][0];
             const secondPlayer = content["O"][1];
@@ -166,9 +200,24 @@ function connect(header) {
             )
                 return;
 
+            // If estimation isn't available, this means we don't know much about the attack yet
+            // Need to store the data and update it when we get the real data
+
             const estimatedNumberOfTroops = content["M"][0]["GS"];
-            let numberOfTroops = null;
-            let numberOfTools = null;
+            let numberOfTroops = {
+                total: 0,
+                left: 0,
+                right: 0,
+                middle: 0,
+                courtyard: 0,
+            };
+            let numberOfTools = {
+                total: 0,
+                left: 0,
+                right: 0,
+                middle: 0,
+                courtyard: 0,
+            };
 
             if (!estimatedNumberOfTroops && content["M"][0]["GA"]) {
                 const leftSide = content["M"][0]["GA"]["L"];
@@ -176,23 +225,28 @@ function connect(header) {
                 const middleSide = content["M"][0]["GA"]["M"];
                 const courtyard = content["M"][0]["GA"]["RW"];
 
-                function getAmount(item) {
+                function getAmount(item, side) {
                     const id = item[0];
                     const number = item[1];
 
-                    if (soldiers[id]) {
-                        numberOfTroops += number;
-                    } else if (tools[id]) {
-                        numberOfTools += number;
+                    const isSoldier = soldiers.find((soldier) => soldier.id === id);
+                    const isTool = tools.find((tool) => tool.id === id);
+
+                    if (isSoldier) {
+                        numberOfTroops.total += number;
+                        numberOfTroops[side] += number;
+                    } else if (isTool) {
+                        numberOfTools.total += number;
+                        numberOfTools[side] += number;
                     } else {
                         console.log(`Unknown soldier/tool: ${id}. With number: ${number}`);
                     }
                 }
 
-                leftSide.forEach((item) => getAmount(item));
-                rightSide.forEach((item) => getAmount(item));
-                middleSide.forEach((item) => getAmount(item));
-                courtyard.forEach((item) => getAmount(item));
+                leftSide.forEach((item) => getAmount(item, "left"));
+                rightSide.forEach((item) => getAmount(item, "right"));
+                middleSide.forEach((item) => getAmount(item, "middle"));
+                courtyard.forEach((item) => getAmount(item, "courtyard"));
             }
 
             const travelTime = content["M"][0]["M"]["TT"]; // in seconds
@@ -205,19 +259,42 @@ function connect(header) {
             const date = new Date();
             date.setSeconds(date.getSeconds() + timeToLand);
 
-            const webhookUrl = process.env.WEBHOOK_URL;
+            const arrivalDateRelative = `<t:${Math.floor(date.getTime() / 1000)}:R>`;
+
+            const notKnownDescription = `
+                Nombre de troupes estimé: ${estimatedNumberOfTroops}
+
+                Alliance: ${attackingAlliance || "Sans Alliance"}
+                
+                Temps restant: ${timeRemaining}
+                Date d'arrivée: ${arrivalDateRelative}
+            `;
+
+            const knownDescription = `
+                Alliance: ${attackingAlliance || "Sans Alliance"}
+
+                Nombre total de troupes: ${numberOfTroops.total}
+                Nombre total d'engins: ${numberOfTools.total}
+
+                Troupes flanc gauche: ${numberOfTroops.left}
+                Engins flanc gauche: ${numberOfTools.left}
+
+                Troupes flanc droit: ${numberOfTroops.right}
+                Engins flanc droit: ${numberOfTools.right}
+
+                Troupes front: ${numberOfTroops.middle}
+                Engins front: ${numberOfTools.middle}
+
+                Troupes cour: ${numberOfTroops.courtyard}
+                Engins cour: ${numberOfTools.courtyard}
+
+                Temps restant: ${timeRemaining}
+                Date d'arrivée: ${arrivalDateRelative}
+            `;
 
             const embed = {
                 title: `${attackedUsername} se fait attaquer par ${attackingUsername}`,
-                description: estimatedNumberOfTroops
-                    ? `Nombre de troupes estimé: ${estimatedNumberOfTroops}\nAlliance: ${attackingAlliance}\n\nTemps restant: ${timeRemaining}\nDate d'arrivée: ${date.toLocaleString(
-                          "fr-FR"
-                      )}`
-                    : `Nombre de troupes: ${
-                          numberOfTroops || "NA"
-                      }\nNombre d'outils: ${"NA"}\nAlliance: ${attackingAlliance}\n\nTemps restant: ${timeRemaining}\nDate d'arrivée: ${date.toLocaleString(
-                          "fr-FR"
-                      )}`,
+                description: estimatedNumberOfTroops ? notKnownDescription : knownDescription,
                 color: 14427686,
             };
 
@@ -225,7 +302,7 @@ function connect(header) {
 
             // if can find player, send a ping
             const player = players.find((player) => player.username === attackedUsername);
-            if (player) {
+            if (player && (!player.minTroops || numberOfTroops >= player.minTroops)) {
                 message = `<@${player.discordId}>`;
             }
 
@@ -235,7 +312,7 @@ function connect(header) {
             };
 
             try {
-                const res = await fetch(webhookUrl, {
+                const res = await fetch(webhookUrlAttacks, {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json",
@@ -251,21 +328,24 @@ function connect(header) {
             }
         }
 
-        if (response.command === "acm") {
+        if (data.command === "acm") {
             // Alliance chat message
-            const content = JSON.parse(response.content);
+            return;
+
+            const content = JSON.parse(data.content);
 
             const sender = content["CM"]["PN"];
             const message = sanitize(content["CM"]["MT"]);
-
-            const webhookUrl = process.env.WEBHOOK_URL;
 
             const webhookData = {
                 content: `**${sender}**: ${message}`,
             };
 
+            const threadId = "1191570871995879474";
+
             try {
-                const res = await fetch(webhookUrl, {
+                // Use string params for threadId
+                const res = await fetch(`${webhookUrlAllianceChat}?thread_id=${threadId}`, {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json",
@@ -275,14 +355,22 @@ function connect(header) {
 
                 if (res.status !== 204) {
                     console.log("Error while sending message to Discord.");
+                    console.log(await res.text());
                 }
             } catch (error) {
                 console.log(error);
             }
         }
 
-        if (response.command === "all") {
-            const content = JSON.parse(response.content);
+        if (data.command === "all") {
+            // Alliance logs
+            return;
+
+            const content = JSON.parse(data.content);
+
+            // 0 - Joined alliance
+            // 1 - Left alliance
+            // 2 - Kicked from alliance
             const logs = content["AL"].filter(
                 (log) => log["A"] === 0 || log["A"] === 1 || log["A"] === 2
             );
